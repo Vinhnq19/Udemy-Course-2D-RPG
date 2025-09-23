f898b98ca58fa3b331c9fa3bf2ac524e102d5779
Quang Vinh <nguyenquangvinh5219@gmail.com>
2025-09-23 15:33:12 +0700

    Enemy Attack State

---
+++ Assets/Script/PlayerStates/Entity_AnimationTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Entity_AnimationTrigger : MonoBehaviour
+{
+    private Entity entity;
+    private void Awake()
+    {
+        entity = GetComponentInParent<Entity>();
+    }
+    private void CurrentStateTrigger()
+    {
+        entity.CallAnimationTrigger();
+    }
+}
